7334ba2fb06a75cae2b6bea5965e7d290b90da99
hubaek <zjfl3122@naver.com>
2025-01-17 20:53:04 +0900

    initial commit

---
+++ src/main/java/com/onboading/intellipick/BackendOnboadingApplication.java
@@ -0,0 +1,13 @@
+package com.onboading.intellipick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendOnboadingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendOnboadingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/onboading/intellipick/BackendOnboadingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onboading.intellipick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendOnboadingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
