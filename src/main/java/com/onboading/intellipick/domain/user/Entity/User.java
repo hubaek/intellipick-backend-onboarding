1ab52d6eddc63cbc99eb049b456ff21d1bf955f8
hubaek <zjfl3122@naver.com>
2025-01-20 02:45:42 +0900

    Feat : User Entity 추가

---
+++ src/main/java/com/onboading/intellipick/domain/user/Entity/User.java
@@ -0,0 +1,30 @@
+package com.onboading.intellipick.domain.user.Entity;
+
+import com.onboading.intellipick.domain.user.enums.UserRole;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String nickname;
+
+    @Enumerated(EnumType.STRING)
+    private UserRole userRole;
+
+}
