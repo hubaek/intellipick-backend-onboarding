6fc36526d5d554ecf9258651ef299ec16fb8d116
hubaek <zjfl3122@naver.com>
2025-01-20 04:40:19 +0900

    Feat : AuthorityDto 추가

---
+++ src/main/java/com/onboading/intellipick/domain/auth/controller/dto/AuthorityDto.java
@@ -0,0 +1,10 @@
+package com.onboading.intellipick.domain.auth.controller.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class AuthorityDto {
+    private String authorityName;
+}
